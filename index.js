df6c0e86b64efde590b875b0bf8eda522743ddd7
Dominique Walker <DW629@live.mdx.ac.uk>
2022-01-31 23:27:41 +0000

    heroku

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ const posts = require('./routes/api/posts')
 
 app.use('/api/posts', posts)
 
-if(process.env.NODE_ENV === 'production') {
 
-app.use(express.static(__dirname + '/public/'))
-
-
-app.get(/.*/, (req,res) => res.sendFile(__dirname + 'client/public/index.html/'))
-}
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log('server started on port 8000'));
