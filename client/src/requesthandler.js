8a775b77e24f8335b0817d72b8f03fe748a813e9
Dominique Walker <DW629@live.mdx.ac.uk>
2022-01-31 21:49:16 +0000

    Heroku fixing

---
+++ client/src/requesthandler.js
@@ -0,0 +1,43 @@
+
+fetch('http://localhost:8000/api/posts/').then(
+
+function (response) {
+    response.json().then( // use .json() instead of .text()
+    function(json) {
+    alert(JSON.stringify(json));
+
+    });
+    if (!response.ok) {
+        throw Error(response.statusText)
+      }
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+    })
+    .catch(error => console.error(error))
+
+
+    
+    fetch('http://localhost:8000/api/posts/').then(
+ {
+  method: "post",
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+  },
+
+  //make sure to serialize your JSON body
+  body: JSON.stringify({
+    Firstname: Firstname,
+    Surname: Surname,
+    Phonenumber: Phonenumber,
+    Lessonid: Lessonid,
+    Spaces: Spaces
+  })
+})
+.then( (response) => { 
+   //do something awesome that makes the world a better place
+});
+
+export default requesthandeler; 
